1d31d709c9f1db37438f937a6c47319bc7127a58
MohammadrezaDindarloo <M.dindarloo@email.kntu.ac.ir>
2025-01-18 16:34:35 +0330

    result updated

--- scampi_factor_graph/result/VKF.py
+++ scampi_factor_graph/result/VKF.py
@@ -13,13 +13,19 @@ with open("result/calibration_result.csv", 'r') as file:
 before_vkf = [float(row[0]) for row in rows]
 after_vkf = [float(row[1]) for row in rows]
 
+print("before_vkf: ", sum(before_vkf) / len(before_vkf))
+print("after_vkf", sum(after_vkf) / len(after_vkf))
+
+
 plt.plot(before_vkf,'r*')
 plt.plot(after_vkf,'bo')
 plt.title("Pose refinement error")
 plt.legend(['Before refinement', 'After refinement'])
 plt.xlabel("Pose number")
-plt.ylabel("Position error")
+plt.ylabel("Position error(m)")
 plt.savefig("PositionError.pdf")
 plt.show()
+
+
 
 
